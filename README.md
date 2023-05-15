a53583d0f9befbae4eea0ea43846e9317f43f736
fajarkazam <44472680+fajarkazam@users.noreply.github.com>
2023-05-15 14:12:13 +0700

    Changes

--- README.md
+++ README.md
@@ -15,6 +15,11 @@
     <img src="https://badgen.net/badge/Gatsby/2.23.12/purple" alt="Gatsby version" />
   </a>
   
+   <a href="https://github.com/fajarkazam/socmedproject/pulls">
+     <img src="https://badgen.net/github/prs/fajarkazam/socmedproject" alt="Pull Request Amount">
+   </a>
+   <img src="https://badgen.net/github/last-commit/fajarkazam/socmedproject" alt="Last commit date" />
+    
   <a href="./LICENSE">
     <img src="https://badgen.net/badge/license/MIT/blue" alt="License">
   </a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -739,7 +739,12 @@ export default (() => {
 			<style place={"endOfHead"} rawKey={"6461957b820441001fb09aeb"}>
 				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
 			</style>
-			<script place={"endOfHead"} rawKey={"6461999ed6a6c13598cb54e0"} />
+			<script place={"endOfHead"} rawKey={"6461999ed6a6c13598cb54e0"}>
+				{"<!-- Google Tag Manager -->\n<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\nnew Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\nj=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n})(window,document,'script','dataLayer','GTM-T44CF6S');</script>\n<!-- End Google Tag Manager -->"}
+			</script>
+			<script place={"endOfBody"} rawKey={"6461db050f22563350f49e1e"}>
+				{"<!-- Google Tag Manager (noscript) -->\n<noscript><iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-T44CF6S\"\nheight=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>\n<!-- End Google Tag Manager (noscript) -->"}
+			</script>
 		</RawHtml>
 	</Theme>;
 });
