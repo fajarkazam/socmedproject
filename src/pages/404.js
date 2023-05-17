414b60f9edc6de3f050968aa8bfc3d90ab15c5e6
fajarkazam <44472680+fajarkazam@users.noreply.github.com>
2023-05-17 14:03:36 +0700

    Changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -42,7 +42,13 @@ export default (() => {
 			Made on Quarkly
 		</Link>
 		<RawHtml>
-			<script async={false} place={"endOfHead"} rawKey={"6461999ed6a6c13598cb54e0"}>
+			<script
+				async={true}
+				type={""}
+				defer={true}
+				place={"endOfHead"}
+				rawKey={"6461999ed6a6c13598cb54e0"}
+			>
 				{"<!-- Google Tag Manager -->\n<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\nnew Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\nj=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n})(window,document,'script','dataLayer','GTM-T44CF6S');</script>\n<!-- End Google Tag Manager -->"}
 			</script>
 			<script place={"endOfBody"} rawKey={"6463be38dcb31f0ec619609f"}>
